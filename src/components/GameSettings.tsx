db0919c1cf88136a537a2c566cbb79f128af7c1e
Henry Chen <henchen@seas.upenn.edu>
2022-10-05 16:32:36 -0400

    Game settings and Lobby comps

--- src/components/GameSettings.tsx
+++ src/components/GameSettings.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 
-const GameSettings = (lobbyid: string): JSX.Element => {
-  return (
-    <>
-      <h1> Game Settings</h1>
-      <div>
-        <h2> Invite friends</h2>
+import { LobbySettings, GameSpeed } from '@backend/lobby';
 
-        {/* TODO: have input be placeholder for url, readonly and copy to clipboard */}
-        <input type="text"/> 
+export default class GameSettings extends React.Component<
+  { lobbyid: string },
+  { settings: LobbySettings }
+> {
+  constructor(props: { lobbyid: string }) {
+    super(props);
+    this.state = {
+      settings: {
+        isPrivate: false,
+        hideBankCards: false,
+        gameSpeed: GameSpeed.Medium,
+      },
+    };
+  }
 
-        <h2> Private Game </h2>
+  render() {
+    return (
+      <>
+        <h1> Game Settings</h1>
+        <div>
+          <h2> Invite friends with this code: {this.props.lobbyid} </h2>
 
-        <h2> Hide Bank Cards</h2>
+          {/* TODO: have input be placeholder for url, readonly and copy to clipboard */}
+          <input type="text" />
 
-        <h2> I'm Ready </h2>
+          <h2> Private Game </h2>
+          <input type="checkbox" onChange={e => this.setState({ settings: {...this.state.settings, isPrivate: !this.state.settings.isPrivate}})} />
 
-        {/* start game button for only p1 */}
-      </div>
-    </>
-  );
-};
+          <h2> Hide Bank Cards</h2>
+          <input type="checkbox" onChange={e => this.setState({ settings: {...this.state.settings, hideBankCards: !this.state.settings.hideBankCards}})} />
 
-export default GameSettings;
+          <h2> Game Speed </h2>
+
+          <h2> I'm Ready </h2>
+          {/* start game button shows only for p1 */}
+        </div>
+      </>
+    );
+  }
+}
--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const Lobby = () => {
-  // const [isPrivate, setIsPrivate] = useState(false);
+import GameSettings from '../components/GameSettings';
+import LobbyChat from '../components/LobbyChat';
 
-  // useEffect(() => {
-  //   const { cc } = useContext();
-  //   setIsPrivate(cc.getLobbySettings().isPrivate);
-  //   cc.addOnLobbySettingsChangeListener((lobbySettings) => {
-  //     setIsPrivate(lobbySettings.isPrivate);
-  //   });
-  // }, []);
+export default class Lobby extends React.Component<{}> {
+  render() {
+    return (
+      <>
+        <h1>Lobby</h1>
 
-  const { lobbyid } = useParams();
-  return <h1> Lobby ID: {lobbyid} </h1>;
-};
+        <GameSettings lobbyid='dev' />
+        <hr />
+        <LobbyChat />
+      </>
+    );
+  }
+}
+// const Lobby = () => {
+//   // const [isPrivate, setIsPrivate] = useState(false);
 
-export default Lobby;
+//   // useEffect(() => {
+//   //   const { cc } = useContext();
+//   //   setIsPrivate(cc.getLobbySettings().isPrivate);
+//   //   cc.addOnLobbySettingsChangeListener((lobbySettings) => {
+//   //     setIsPrivate(lobbySettings.isPrivate);
+//   //   });
+//   // }, []);
+
+//   const { lobbyid } = useParams();
+//   return <h1> Lobby ID: {lobbyid} </h1>;
+// };
+
+// export default Lobby;
