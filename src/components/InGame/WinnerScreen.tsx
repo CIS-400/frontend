7b84462d6b48c7cbfdca9e2d15ead78a69664361
tom-fanelli <tfanelli15@gmail.com>
2023-04-16 19:34:35 -0400

    winner screen

--- src/components/InGame/WinnerScreen.tsx
+++ src/components/InGame/WinnerScreen.tsx
@@ -1,10 +1,20 @@
 import { useContext } from 'react'
 import { AppContext } from 'src/store'
+import './WinnerScreen.css'
 
 const WinnerScreen = () => {
   const [state] = useContext(AppContext)
   return (
-    <div>The winner is: {state.lobby.players[state.lobby.winner!].name}</div>
+    <div className='winner-screen-container'>
+      <div className='winner-screen-message'>
+        The winner is: tom!
+        {/* The winner is: {state.lobby.players[state.lobby.winner!].name} */}
+      </div>
+      <div className="winner-screen-message-2">
+        Thanks for playing settlers.io! 
+      </div>
+    </div>
+    
   )
 }
 
