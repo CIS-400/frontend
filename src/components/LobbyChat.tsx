8e5a9cfa707f77f2aee4c34e729558497cc522c5
tom-fanelli <tfanelli15@gmail.com>
2023-04-16 17:11:29 -0400

    final styling

--- src/components/LobbyChat.tsx
+++ src/components/LobbyChat.tsx
@@ -2,28 +2,32 @@ import React, { useContext } from 'react'
 import { AppContext, AppStateAction } from '../store'
 import ChatInput from './ChatInput'
 import lobbyContext from 'src/lobby-context'
+import './LobbyChat.css'
 
 const LobbyChat = (props: { }) => {
   const [state, dispatch] = useContext(AppContext)
 
   return (
     <>
-      <div>
+      <div className='chat-messages'>
         {state.lobby.chat.map(({ sender, message }, i) => (
-          <div key={i}>
-            {sender}: {message}
+          <div className='chat-message' key={i}>
+            <span className='chat-sender'>{sender}:</span> {message}
           </div>
         ))}
       </div>
-      <ChatInput
-        onSend={(message) => {
-          dispatch({
-            type: AppStateAction.SendChatMessage,
-            payload: { pid: lobbyContext.clientController.pid, message },
-          })
-          lobbyContext.clientController.sendChatMessage(message)
-        }}
-      />
+      <div className='chat-input-container'>
+        <span>Send a Chat:</span>
+        <ChatInput
+          onSend={(message) => {
+            dispatch({
+              type: AppStateAction.SendChatMessage,
+              payload: { pid: lobbyContext.clientController.pid, message },
+            })
+            lobbyContext.clientController.sendChatMessage(message)
+          }}
+        />
+      </div>
     </>
   )
 }
