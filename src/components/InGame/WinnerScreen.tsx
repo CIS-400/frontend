005b32f6ed6a62e5a3e04b7317c0db3f6c5fd793
Henry Chen <henchen@seas.upenn.edu>
2023-04-16 19:42:41 -0400

    remove hardcode winner

--- src/components/InGame/WinnerScreen.tsx
+++ src/components/InGame/WinnerScreen.tsx
@@ -7,8 +7,8 @@ const WinnerScreen = () => {
   return (
     <div className='winner-screen-container'>
       <div className='winner-screen-message'>
-        The winner is: tom!
-        {/* The winner is: {state.lobby.players[state.lobby.winner!].name} */}
+        {/* The winner is: tom! */}
+        The winner is: {state.lobby.players[state.lobby.winner!].name}
       </div>
       <div className="winner-screen-message-2">
         Thanks for playing settlers.io! 
